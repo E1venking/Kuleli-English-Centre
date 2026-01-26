d77d1e3299c0d73f962c466f9d73a573668e6b7f
E1venking <161348418+E1venking@users.noreply.github.com>
2026-01-26 13:58:23 +0300

    Delete kuleli-english-centre (1) directory

--- kuleli-english-centre (1)/constants/assets.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Local assets for Kuleli Conversation Lab.
- * Removed Base64 assets to maintain a lightweight, icon-based UI.
- */
-
-export const MSU_LOGO_BASE64 = "";
-export const YDYO_LOGO_BASE64 = "";
